91d396fb6028e1bdb942c16b79db8f4b8cdac335
Darek <mrocznaklawiatura@gmail.com>
2020-03-12 11:28:00 +0100

    missing file from previous commit

---
+++ Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace NoteApp
+{
+    static class Logger
+    {
+        private const string LOG_FILE_PATH = "errors.log";
+        public static void PrintError(string content)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Beep();
+            Console.WriteLine($"ERR: {content}");
+            Console.ForegroundColor = ConsoleColor.White;
+            File.AppendAllText(LOG_FILE_PATH, $"#{DateTime.Now}: {content}\n");
+        }
+    }
+}
